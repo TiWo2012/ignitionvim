518e3d4852e1fc687ce5e7a4899d29a07a1655b0
TiWo2012 <timo.wolf@gmx.ch>
2025-02-15 12:25:17 +0100

    keymap change

--- lua/plugins/tabs.lua
+++ lua/plugins/tabs.lua
@@ -2,7 +2,7 @@ return {
   {
     "leath-dub/snipe.nvim",
     keys = {
-      {"<leader>gb", function () require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu"}
+      {"<leader>so", function () require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu"}
     },
     opts = {}
   },
